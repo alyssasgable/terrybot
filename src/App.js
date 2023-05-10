34ade984725a7315cebe57d4d228ec1d3fb074fa
Alyssa Gable <alyssasgable@gmail.com>
2023-05-09 20:32:03 -0400

    styled the side bar

--- src/App.js
+++ src/App.js
@@ -6,13 +6,29 @@ const App = () => {
       <section className="side-bar">
         <button className="button">+ New Chat</button>
         <ul className="history"></ul>
+          <li>
+            Test
+          </li>
         <nav>
           <p>Made By The Culture</p>
         </nav>
       </section>
 
       <section className="main">
-      
+        <img className="logo" src={logo} />
+        <h1>terryGPT</h1>
+        <ul className="feed"></ul>
+
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts from events after 2021. 
+            ChatGPT May 4
+          </p>
+        </div>
       </section>
 
     </div>
