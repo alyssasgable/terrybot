d3d797f3143896812d0a4ca520c8c43e982eff96
Alyssa Gable <alyssasgable@gmail.com>
2023-05-10 10:07:54 -0400

    added getMessages function to the front end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 
 const App = () => {
+  
+    const getMessages = async () => {
+
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+            message: "My mic sounds nice check 2"
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      
+      try {
+       const response = await fetch('http://localhost:8000/completions', options)
+       const data = await response.json()
+       console.log(data)
+
+      } catch (error) {
+          console.error(error)
+      }
+    }
   return (
     <div className="app">
       <section className="side-bar">
         <button className="button">+ New Chat</button>
         <ul className="history"></ul>
           <li>
-            Test
+            History ...
           </li>
         <nav>
           <p>Made By The Culture</p>
@@ -25,7 +47,7 @@ const App = () => {
         <div className="bottom-section">
           <div className="input-container">
             <input/>
-            <div id="submit">➢</div>
+            <div id="submit" onClick={getMessages}> ➢</div>
           </div>
           <p className="info">
             Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts from events after 2021. 
