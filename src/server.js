e68f5746c2864afadadcb49eee7b1bc762596f13
Alyssa Gable <alyssasgable@gmail.com>
2023-05-10 11:03:39 -0400

    connected frontend to backend api

--- src/server.js
+++ src/server.js
@@ -8,4 +8,28 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const API_KEY = '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'
+
+app.post('/completions', async (req, res) => {
+    const options = {
+        method: "POST", 
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model : "gpt-3.5-turbo",
+            messages : [{role: "user", content: "My mic sounds nice, check server.js"}],
+            max_tokens: 100,
+        })
+    }
+    try { 
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
 app.listen(PORT, () => console.log('Horray! Your server is running on PORT ' + PORT))
