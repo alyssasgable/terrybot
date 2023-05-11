5960871f7bc1118887fbb0cee51c0ed91cd00a28
Alyssa Gable <alyssasgable@gmail.com>
2023-05-11 08:21:13 -0400

    Revert "fixed gitignore file"
    This reverts commit a63b51200e6f710bc71c311fb63eea37a7c9f9dd.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import logo from './logo.svg';
-import {useState, useEffect} from 'react';
 
 const App = () => {
-    const [ value, setValue] = useState(null)
-    const [message, setMessage] = useState(null)
-    const [previousChats, setPreviousChats] = useState([])
-    const [currentTitle, setCurrentTitle] = useState(null)
-
-    const createNewChat = () => {
-      setMessage(null)
-      setValue("")
-      setCurrentTitle(null)
-    }
-
-    const handleClick = (uniqueTitle) => {
-      setCurrentTitle(uniqueTitle)
-      setMessage(null)
-      setValue("")
-    }
-
+  
     const getMessages = async () => {
 
       const options = {
         method: "POST",
         body: JSON.stringify({
-            message: value
+            message: "My mic sounds nice check 2"
           }),
           headers: {
             "Content-Type": "application/json"
@@ -34,47 +17,20 @@ const App = () => {
       try {
        const response = await fetch('http://localhost:8000/completions', options)
        const data = await response.json()
-       setMessage(data.choices[0].message)
+       console.log(data)
 
       } catch (error) {
           console.error(error)
       }
     }
-
-    useEffect(() => {
-
-      console.log(currentTitle, value, message)
-
-      if (!currentTitle && value && message) {
-        setCurrentTitle(value)
-      }
-      if (currentTitle && value && message ) {
-        setPreviousChats(prevChats => (
-          [...prevChats,
-            {
-              title: currentTitle,
-              role: "role",
-              content: value
-            },
-            {
-              title: currentTitle,
-              role: message.role,
-              content: message.content
-            }
-          ]
-        ))
-      }
-    }, [message, currentTitle])
-
-    const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle)
-    const uniqueTitles = Array.from(new (previousChats.map(previousChat => previousChat.title)))
-
   return (
     <div className="app">
       <section className="side-bar">
-        <button onClick={createNewChat} className="button">+ New Chat</button>
+        <button className="button">+ New Chat</button>
         <ul className="history"></ul>
-          {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick({uniqueTitle})}>{uniqueTitle}</li>)}
+          <li>
+            History ...
+          </li>
         <nav>
           <p>Made By The Culture</p>
         </nav>
@@ -83,19 +39,14 @@ const App = () => {
       <section className="main">
         <div className="header">
           <img className="logo" src={logo} />
-          {!currentTitle && <h1>terryGPT</h1>}
+          <h1>terryGPT</h1>
         </div>
        
-        <ul className="feed">
-          {currentChat?.map((chatMessage, index) => <li key={index}>
-            <p className="role">{chatMessage.role}</p>
-            <p>{chatMessage.message}</p>
-          </li>)}
-        </ul>
+        <ul className="feed"></ul>
 
         <div className="bottom-section">
           <div className="input-container">
-            <input value={value} onChange= {(e) => setValue(e.target.value)}/>
+            <input/>
             <div id="submit" onClick={getMessages}> ➢</div>
           </div>
           <p className="info">
